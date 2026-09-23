8d5c222a123fc54264959dd0482fc7b89e945916
github-action-benchmark <github@users.noreply.github.com>
2026-09-23 08:12:25 +0000

    add Rust / S001 (opentelemetry 0.33.0) (customSmallerIsBetter) benchmark result for 1de34a1ab63ee98ab3de2d407221da3e5d98c1e9

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1790151081199,
+  "lastUpdate": 1790151145065,
   "repoUrl": "https://github.com/open-telemetry/opentelemetry-benchmarks",
   "entries": {
     ".NET / S001 (.NET 10.0.11)": [
@@ -612,6 +612,36 @@ window.BENCHMARK_DATA = {
             "value": 11.878381733658644,
             "unit": "ns",
             "extra": "runner=ubuntu24\nruntime=rustc 1.98.1 (48a229cea 2026-09-01)\ncpu=AMD EPYC 7763 64-Core Processor\nkernel=6.17.0-1022-azure\nframework=Criterion 0.8.2"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "29139614+renovate[bot]@users.noreply.github.com",
+            "name": "renovate[bot]",
+            "username": "renovate[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "1de34a1ab63ee98ab3de2d407221da3e5d98c1e9",
+          "message": "Bump dependency ubuntu to v26 (#52)\n\n| datasource     | package | from  | to    |\n| -------------- | ------- | ----- | ----- |\n| github-runners | ubuntu  | 24.04 | 26.04 |\n\nSigned-off-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>\nCo-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>",
+          "timestamp": "2026-09-23T09:10:19+01:00",
+          "tree_id": "a8aea312b2a444d671af49b139682975a5fe0c2a",
+          "url": "https://github.com/open-telemetry/opentelemetry-benchmarks/commit/1de34a1ab63ee98ab3de2d407221da3e5d98c1e9"
+        },
+        "date": 1790151144592,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "counter increment",
+            "value": 11.895112132667165,
+            "unit": "ns",
+            "extra": "runner=ubuntu26\nruntime=rustc 1.98.1 (48a229cea 2026-09-01)\ncpu=AMD EPYC 7763 64-Core Processor\nkernel=7.0.0-1012-azure\nframework=Criterion 0.8.2"
           }
         ]
       }
